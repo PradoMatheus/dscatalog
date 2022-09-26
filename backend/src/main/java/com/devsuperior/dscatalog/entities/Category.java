261d855f1dc0b395c29f43502c3e7de39be0388e
PradoMatheus <pradomatheus.19#hotmail.com>
2022-09-26 20:06:35 -0300

    h2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dscatalog.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
